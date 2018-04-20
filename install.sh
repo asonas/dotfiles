75991ba174f8008d97f53d29ff4daedf681bb607
Yuya Fujiwara <asonas@cookpad.com>
2018-04-20 15:29:51 +0900

    Add karabiner

--- install.sh
+++ install.sh
@@ -12,3 +12,4 @@ mkdir $HOME/bin
 ln -Fis "$PWD/bin/video_rahmen" $HOME/bin
 ln -Fis "$PWD/bin/loadavg.sh" $HOME/bin
 ln -Fis $PWD/.config/nvim $HOME/.config/nvim
+ln -Fis $PWD/.config/karabiner $HOME/.config/karabiner
