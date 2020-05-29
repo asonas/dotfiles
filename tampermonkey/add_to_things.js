48fe327f25f6da437f9201c98d664945ed4f310f
Yuya Fujiwara <asonas@cookpad.com>
2020-05-29 21:33:53 +0900

    Add tampermonkey scripts

---
+++ tampermonkey/add_to_things.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         ToDo to Things from GHE Issues
+// @namespace   https://ason.as/
+// @version      0.1
+// @description  todo to thingsapp
+// @author       asonas
+// @match        https://ghe.ckpd.co/*
+// @grant GM_setValue
+// @grant GM_getValue
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var currentUrl = encodeURI(location.href);
+    var title = document.querySelector('.js-issue-title').innerHTML.trim();
+    var thingsSchemaUri = 'things:///add?title='+title+'&notes='+currentUrl+'&when=today'
+
+    var style = 'background-color: #2174E5; background-image: linear-gradient(-180deg, #2174E5, #2274E5 90%); color: #fff;'
+    var html = '<a style="'+style+'" href="'+thingsSchemaUri+'" class="btn btn-sm" aria-expanded="false" aria-label="Add Thingsapp">Add to Things</a>'
+
+    var ele = document.querySelector('.gh-header-actions');
+    ele.insertAdjacentHTML('afterbegin', html)
+})();
