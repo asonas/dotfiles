9295f1c77b8550fd302707be4e226f37784334b5
Yuya Fujiwara <asonas@cookpad.com>
2020-06-16 11:40:32 +0900

    Encode issue title

--- tampermonkey/add_to_things.js
+++ tampermonkey/add_to_things.js
@@ -13,7 +13,7 @@
     'use strict';
     var currentUrl = encodeURI(location.href);
     var title = document.querySelector('.js-issue-title').innerHTML.trim();
-    var thingsSchemaUri = 'things:///add?title='+title+'&notes='+currentUrl+'&when=today'
+    var thingsSchemaUri = 'things:///add?title='+encodeURI(title)+'&notes='+currentUrl+'&when=today'
 
     var style = 'background-color: #2174E5; background-image: linear-gradient(-180deg, #2174E5, #2274E5 90%); color: #fff;'
     var html = '<a style="'+style+'" href="'+thingsSchemaUri+'" class="btn btn-sm" aria-expanded="false" aria-label="Add Thingsapp">Add to Things</a>'
