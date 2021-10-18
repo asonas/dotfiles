81fc53943cbfce671c39dfea12567afd1fa945e5
Yuya Fujiwara <asonas@cookpad.com>
2021-10-18 11:37:07 +0900

    Adjust style

--- tampermonkey/add_to_things.js
+++ tampermonkey/add_to_things.js
@@ -15,7 +15,7 @@
     var title = document.querySelector('.js-issue-title').innerHTML.trim();
     var thingsSchemaUri = 'things:///add?title='+encodeURI(title)+'&notes='+currentUrl+'&when=today'
 
-    var style = 'background-color: #2174E5; background-image: linear-gradient(-180deg, #2174E5, #2274E5 90%); color: #fff;'
+    var style = 'background-color: #2174E5; background-image: linear-gradient(-180deg, #2174E5, #2274E5 90%); color: #fff; margin-right: 4px;'
     var html = '<a style="'+style+'" href="'+thingsSchemaUri+'" class="btn btn-sm" aria-expanded="false" aria-label="Add Thingsapp">Add to Things</a>'
 
     var ele = document.querySelector('.gh-header-actions');
