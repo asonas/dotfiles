d97324202251482f2c1108c5d95fda6f4990975d
Yuya Fujiwara <asonas@cookpad.com>
2022-03-18 17:22:40 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,3 +13,4 @@ ln -Fis "$PWD/bin/video_rahmen" $HOME/bin
 ln -Fis "$PWD/bin/loadavg.sh" $HOME/bin
 ln -Fis $PWD/.config/nvim $HOME/.config/nvim
 ln -Fis $PWD/.config/karabiner $HOME/.config/karabiner
+ln -Fis $PWD/.config/starship.toml $HOME/.config/starship.toml
