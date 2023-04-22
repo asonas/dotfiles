9678504ccb91454b683be27f69912ae1e3f01caa
Yuya Fujiwara <asonas@cookpad.com>
2023-04-22 15:28:11 +0900

    update wezterm conf

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,39 +1,39 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 config.keys = {
-    {
-      key = 'c',
-      mods = 'CTRL',
-      action = wezterm.action.CopyTo 'ClipboardAndPrimarySelection',
-    },
-    {
-      key = 'v',
-      mods = 'CTRL',
-      action = wezterm.action.PasteFrom 'Clipboard',
-    },
-    {
-      key = 'v',
-      mods = 'CTRL',
-      action = wezterm.action.PasteFrom 'PrimarySelection',
-    },
-    {
-      key = 'g',
-      mods = 'CTRL',
-      action = wezterm.action.SendKey { key = 'Escape' },
-    },
-    {
-      key = 'R',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.ReloadConfiguration,
-    },
+	{
+		key = "c",
+		mods = "CTRL",
+		action = wezterm.action.CopyTo("ClipboardAndPrimarySelection"),
+	},
+	{
+		key = "v",
+		mods = "CTRL",
+		action = wezterm.action.PasteFrom("Clipboard"),
+	},
+	{
+		key = "v",
+		mods = "CTRL",
+		action = wezterm.action.PasteFrom("PrimarySelection"),
+	},
+	{
+		key = "g",
+		mods = "CTRL",
+		action = wezterm.action.SendKey({ key = "Escape" }),
+	},
+	{
+		key = "R",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ReloadConfiguration,
+	},
 }
 
-config.font = wezterm.font_with_fallback {
-  'SauceCodePro NF',
-  'SauceCodePro Nerd Font',
-}
+config.font = wezterm.font_with_fallback({
+	"SauceCodePro NF",
+	"SauceCodePro Nerd Font",
+})
 
-config.color_scheme = 'Solarized Darcula'
+config.color_scheme = "Solarized Darcula"
 
 return config
