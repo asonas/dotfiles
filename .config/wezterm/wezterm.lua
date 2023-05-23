52ea0b6bad96aa9c86edd45e34c86a111a287a23
Yuya Fujiwara <asonas@cookpad.com>
2023-05-23 15:31:21 +0900

    goodbye tmux!

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,52 @@
 local wezterm = require("wezterm")
 local config = {}
 
+config.leader = { key = "j", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	{
+		key = "v",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SplitPane {
+			direction = "Left",
+			size = { Percent = 50 }
+		}
+	},
+	{
+		key = "s",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SplitVertical
+	},
+	{
+		key = "c",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SpawnTab 'CurrentPaneDomain'
+	},
+	{
+		key = "p",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.MoveTabRelative(-1)
+	},
+	{
+		key = "n",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.MoveTabRelative(1)
+	},
+	{
+		key = "o",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.RotatePanes 'Clockwise'
+	},
+	{
+		key = "w",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.PaneSelect
+	},
+	{
+		key = "i",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.ActivatePaneDirection 'Next'
+	},
+}
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
 	config.keys = {
@@ -43,18 +89,18 @@ else
 	config.font_size = 13.0
 	config.freetype_load_target = "HorizontalLcd"
 	config.foreground_text_hsb = {
-     hue = 1.0,
-     saturation = 1.0,
-     brightness = 0.9,  -- default is 1.0
+		hue = 1.0,
+		saturation = 1.0,
+		brightness = 0.9, -- default is 1.0
 	}
 end
 config.window_background_gradient = {
-  colors = { '#14191e' },
+	colors = { '#14191e' },
 }
 config.bold_brightens_ansi_colors = "BrightAndBold"
 
 config.colors = {
-  foreground = 'white',
+	foreground = 'white',
 
 }
 config.window_background_opacity = 0.92
