85323ab3ec70c6e8a4bf5e6032b97321778fe315
Yuya Fujiwara <asonas@cookpad.com>
2023-06-17 18:11:29 +0900

    Update tmux like config(only macOS)

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,63 +1,72 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.leader = { key = "j", mods = "CTRL", timeout_milliseconds = 1000 }
-config.keys = {
+if wezterm.target_triple == "aarch64-apple-darwin" then
+	config.leader = { key = "j", mods = "CTRL", timeout_milliseconds = 1000 }
+	config.keys = {
+		{
+			key = "v",
+			mods = "LEADER|CTRL",
+			action = wezterm.action.SplitPane({
+				direction = "Right",
+				size = { Percent = 50 },
+			}),
+		},
+		{ key = "s", mods = "LEADER|CTRL", action = wezterm.action.SplitVertical },
+		{
+			key = "c",
+			mods = "LEADER|CTRL",
+			action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+		},
+		{
+			key = "p",
+			mods = "LEADER|CTRL",
+			action = wezterm.action.MoveTabRelative(-1),
+		},
+		{
+			key = "n",
+			mods = "LEADER|CTRL",
+			action = wezterm.action.MoveTabRelative(1),
+		},
+		{
+			key = "o",
+			mods = "LEADER|CTRL",
+			action = wezterm.action.RotatePanes("Clockwise"),
+		},
+		{ key = "w", mods = "LEADER|CTRL", action = wezterm.action.PaneSelect },
+		{
+			key = "i",
+			mods = "LEADER|CTRL",
+			action = wezterm.action.ActivatePaneDirection("Next"),
+		},
+	}
+end
+config.mouse_bindings = {
 	{
-		key = "v",
-		mods = "LEADER|CTRL",
-		action = wezterm.action.SplitPane {
-			direction = "Right",
-			size = { Percent = 50 }
-		}
-	},
-	{
-		key = "s",
-		mods = "LEADER|CTRL",
-		action = wezterm.action.SplitVertical
-	},
-	{
-		key = "c",
-		mods = "LEADER|CTRL",
-		action = wezterm.action.SpawnTab 'CurrentPaneDomain'
-	},
-	{
-		key = "p",
-		mods = "LEADER|CTRL",
-		action = wezterm.action.MoveTabRelative(-1)
-	},
-	{
-		key = "n",
-		mods = "LEADER|CTRL",
-		action = wezterm.action.MoveTabRelative(1)
-	},
-	{
-		key = "o",
-		mods = "LEADER|CTRL",
-		action = wezterm.action.RotatePanes 'Clockwise'
-	},
-	{
-		key = "w",
-		mods = "LEADER|CTRL",
-		action = wezterm.action.PaneSelect
-	},
-	{
-		key = "i",
-		mods = "LEADER|CTRL",
-		action = wezterm.action.ActivatePaneDirection 'Next'
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
 	},
 }
 
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.keys = {
 		{
 			key = "c",
-			mods = "CTRL",
+			mods = "CTRL|SHIFT",
 			action = wezterm.action.CopyTo("ClipboardAndPrimarySelection"),
 		},
 		{
 			key = "v",
-			mods = "CTRL",
+			mods = "CTRL|SHIFT",
 			action = wezterm.action.PasteFrom("Clipboard"),
 		},
 		{
@@ -77,9 +86,7 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
 		},
 	}
 
-	config.font = wezterm.font_with_fallback({
-		"SauceCodePro NF",
-	})
+	config.font = wezterm.font_with_fallback({ "SauceCodePro NF" })
 else
 	config.font = wezterm.font({
 		family = "源ノ角ゴシック Code JP",
@@ -94,14 +101,10 @@ else
 		brightness = 0.9, -- default is 1.0
 	}
 end
-config.window_background_gradient = {
-	colors = { '#14191e' },
-}
+config.window_background_gradient = { colors = { "#14191e" } }
 config.bold_brightens_ansi_colors = "BrightAndBold"
 
-config.colors = {
-	foreground = 'white',
-}
+config.colors = { foreground = "white" }
 config.window_background_opacity = 0.92
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
@@ -112,8 +115,6 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 			max_width
 		)
 	end
-	return {
-		{ Text = tab.tab_index + 1 .. ":" .. title },
-	}
+	return { { Text = tab.tab_index + 1 .. ":" .. title } }
 end)
 return config
