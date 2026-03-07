f56f71893a09b58ac07c9e2a90508e51f8e33734
Yuya Fujiwara <hzw1258@gmail.com>
2026-03-07 23:42:09 +0900

    Update nvim plugins, add Entire CLI completion, and fix install.sh for Linux compatibility

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 cd "$(dirname "$0")"
@@ -44,24 +44,24 @@ do
 
     mkdir -p "$(dirname "$link")"
 
-    ln -Fis "$target" "$link"
+    ln -s "$target" "$link"
 done
 
 [ ! -d "$HOME/bin" ] && mkdir "$HOME/bin"
-ln -Ffs "$PWD/bin/check_sip.sh" "$HOME/bin/check_sip.sh"
-ln -Ffs "$PWD/bin/setup_workspace" "$HOME/bin/setup_workspace"
-ln -Ffs "$PWD/bin/ghro" "$HOME/bin/ghro"
+ln -sf "$PWD/bin/check_sip.sh" "$HOME/bin/check_sip.sh"
+ln -sf "$PWD/bin/setup_workspace" "$HOME/bin/setup_workspace"
+ln -sf "$PWD/bin/ghro" "$HOME/bin/ghro"
 
 case "$OSTYPE" in
   darwin*)
     # macOS
     GIT_PATH=$(brew --prefix git)
-    ln -Ffs "$GIT_PATH/share/git-core/contrib/diff-highlight/diff-highlight" "$HOME/bin/diff-highlight"
+    ln -sf "$GIT_PATH/share/git-core/contrib/diff-highlight/diff-highlight" "$HOME/bin/diff-highlight"
     ;;
   linux*)
     # Linux
     GIT_PATH=$(dirname "$(which git)")
-    ln -Ffs "$GIT_PATH/../share/git/contrib/diff-highlight/diff-highlight" "$HOME/bin/diff-highlight"
+    ln -sf "$GIT_PATH/../share/git/contrib/diff-highlight/diff-highlight" "$HOME/bin/diff-highlight"
     ;;
   *)
     echo "Unsupported OS: $OSTYPE"
