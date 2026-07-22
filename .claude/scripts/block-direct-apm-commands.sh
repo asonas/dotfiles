fc4e1a7a02daeb3867ead634e2d89ea81534e141
Yuya Fujiwara <hzw1258@gmail.com>
2026-07-22 23:09:32 +0900

    Reconstruct quoted APM command tokens
    Skip prefix assignments while preserving valid commands

--- .claude/scripts/block-direct-apm-commands.sh
+++ .claude/scripts/block-direct-apm-commands.sh
@@ -11,7 +11,9 @@ shell_code_only() {
       backtick_sub = 0
       backtick_quote = ""
       command_start = 1
+      command_word = 0
       preserve_quote = 0
+      subcommand_token = 0
     }
     {
       line = $0 "\n"
@@ -32,15 +34,17 @@ shell_code_only() {
           if (ch == "\n") {
             comment = 0
             command_start = 1
+            command_word = 0
+            subcommand_token = 0
             printf "\n"
           } else {
             printf " "
           }
         } else if (quote == "\047") {
           if (ch == quote) {
+            if (!preserve_quote) { printf " " }
             quote = ""
             preserve_quote = 0
-            printf " "
           } else if (preserve_quote) {
             printf "%s", ch
           } else {
@@ -54,6 +58,9 @@ shell_code_only() {
         } else if (ch == "\\") {
           printf "%s", ch
           escaped = 1
+        } else if (command_start && match(substr(line, i), /^[[:alpha:]_][[:alnum:]_]*=[^;&|()[:space:]]*[[:space:]]/)) {
+          for (j = 1; j <= RLENGTH; j++) { printf " " }
+          i += RLENGTH - 1
         } else if (quote != "") {
           if (quote == "\"" && ch == "`") {
             quote = ""
@@ -69,9 +76,9 @@ shell_code_only() {
             i++
           } else {
             if (ch == quote) {
+              if (!preserve_quote) { printf " " }
               quote = ""
               preserve_quote = 0
-              printf " "
             } else if (preserve_quote) {
               printf "%s", ch
             } else {
@@ -85,9 +92,12 @@ shell_code_only() {
           command_start = 1
         } else if (ch == "\"" || ch == "\047") {
           quote = ch
-          preserve_quote = command_start || substr(line, i + 1) ~ ("^(update|install)" ch)
+          quote_is_subcommand = subcommand_token || substr(line, 1, i - 1) ~ /(^|[\/[:space:]])apm[[:space:]]*$/
+          preserve_quote = command_start || command_word || quote_is_subcommand || substr(line, i + 1) ~ ("^(update|install)" ch)
+          if (quote_is_subcommand) { subcommand_token = 1 }
+          if (command_start && preserve_quote) { command_word = 1 }
           if (preserve_quote) { command_start = 0 }
-          printf " "
+          if (!preserve_quote) { printf " " }
         } else if (ch == "#" && (i == 1 || substr(line, i - 1, 1) ~ /[;&|()<>[:space:]]/)) {
           comment = 1
           printf " "
@@ -95,7 +105,13 @@ shell_code_only() {
           printf "%s", ch
           if (ch ~ /[;&|(]/) {
             command_start = 1
-          } else if (ch !~ /[[:space:]]/) {
+            command_word = 0
+            subcommand_token = 0
+          } else if (ch ~ /[[:space:]]/) {
+            command_word = 0
+            subcommand_token = 0
+          } else {
+            if (command_start) { command_word = 1 }
             command_start = 0
           }
         }
@@ -113,7 +129,7 @@ JSON
 input=$(cat)
 cmd=$(printf '%s' "$input" | jq -r '.tool_input.command // empty')
 code=$(printf '%s' "$cmd" | shell_code_only)
-if ! printf '%s' "$code" | grep -Eq '(^|[;&|(])[[:space:]]*([[:alpha:]_][[:alnum:]_]*=[^;&|()[:space:]]*[[:space:]]+)*([^=;&|()[:space:]]*/)?apm[[:space:]]+(update|install)([;&|()<>[:space:]]|$)'; then
+if ! printf '%s' "$code" | grep -Eq '(^|[;&|(])[[:space:]]*([^;&|()[:space:]]*/)?apm[[:space:]]+(update|install)([;&|()<>[:space:]]|$)'; then
   exit 0
 fi
 
